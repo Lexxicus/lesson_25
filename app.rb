d1a51c98141ab0d6c59adea2fd9be7516b6c988e
Lexxicus <alexxxicus@gmail.com>
2019-07-15 13:47:24 +0500

    execute simple select

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'test.sqlite'
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 25000)"
+db.execute "SELECT Name, Price FROM Cars" do |car|
+  puts car
+  puts "====="
+end
 db.close
